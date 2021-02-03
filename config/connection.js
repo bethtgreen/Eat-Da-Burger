fea3b70cfe9467e9a880dc419c16b6b7debf665a
Bethany Green <b.gree10@gail.com>
2021-02-03 14:03:29 -0600

    added jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,18 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-  host: 'localhost',
+var connection;
+if (process.env.JAWSDB_URL){
+connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connecion = mysql.createConnection({
+host: 'localhost',
   port: 8080,
   user: 'root',
   // NOTE: Be sure to add your MySQL password here!
   password: 'rootroot',
   database: 'burger_db',
 });
+};
 
 // Make connection.
 connection.connect((err) => {
